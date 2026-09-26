7451d5bcf53e08b14dbd18c95049a47937f01555
papicesa7jat-cmd <320091572+papicesa7jat-cmd@users.noreply.github.com>
2026-09-26 16:31:00 -0400

    Renueva caché web para corrección de flujo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "flackunloker-offline-web-b43888653869ab48";
+const CACHE_NAME = "flackunloker-offline-web-cup-replicas-20260926";
 const PAGE_FALLBACK = "./index.html";
 
 const APP_SHELL = [
